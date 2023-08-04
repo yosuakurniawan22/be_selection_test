b3bd5bdcb5661eb712a99aa0ccaea0e1501a6c87
yosuakurniawan22 <yosuakurniawan2322@gmail.com>
2023-08-04 18:56:16 +0700

    initialize project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import "dotenv/config";
+import express from "express";
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Test Server"})
+})
+
+const PORT = process.env.PORT || 6000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
