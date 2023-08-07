e1737fc4be26c3b84d7ca046a07935516704281e
yosuakurniawan22 <yosuakurniawan2322@gmail.com>
2023-08-07 22:39:18 +0700

    add cors library

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import express from "express";
 import AuthRoutes from "./routes/auth.route.js";
 import AttendanceRoutes from "./routes/attendance.route.js";
 import PayrollRoutes from "./routes/payroll.route.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json({ message: "Test Server"})
